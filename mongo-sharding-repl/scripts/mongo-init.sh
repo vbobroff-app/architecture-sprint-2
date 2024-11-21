a688458491fe9b0e26238844d3d1af02226cdf38
Pavel Sukharev <psukharev@nntc.pro>
2024-11-21 17:31:01 +0700

    Add mongo-sharding-repl

---
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+###
+# Инициализируем бд
+###
+
+docker compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+EOF
+
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+      ]
+    }
+);
+EOF
+
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+  );
+EOF
+
+
+
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+EOF
+
+docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
+use somedb;
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+
+db.helloDoc.countDocuments();
+EOF
+
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+use somedb;
+
+db.helloDoc.countDocuments();
+EOF
+
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+use somedb;
+
+db.helloDoc.countDocuments();
+EOF
